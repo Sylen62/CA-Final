0ee4a2a5ad052aaa727fc08eb22a77041c13b481
Justas Peldžius <peldjustas@gmail.com>
2022-03-06 14:17:33 +0200

    style: added hover effect to social links container

--- joby/src/components/container/social-links-container.jsx
+++ joby/src/components/container/social-links-container.jsx
@@ -4,32 +4,42 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import { Box, IconButton } from '@mui/material';
+import { useTheme } from '@emotion/react';
 
 const SocialLinksContainer = ({
   linkedIn, facebook, twitter, instagram, iconButtonProps, containerProps,
-}) => (
-  <Box {...containerProps}>
-    {linkedIn ? (
-      <IconButton href={linkedIn} target="blank" {...iconButtonProps}>
-        <LinkedInIcon sx={(theme) => ({ color: theme.palette.secondary.main })} />
-      </IconButton>
-    ) : null}
-    {facebook ? (
-      <IconButton href={facebook} target="blank" {...iconButtonProps}>
-        <FacebookIcon sx={(theme) => ({ color: theme.palette.secondary.main })} />
-      </IconButton>
-    ) : null}
-    {twitter ? (
-      <IconButton href={twitter} target="blank" {...iconButtonProps}>
-        <TwitterIcon sx={(theme) => ({ color: theme.palette.secondary.main })} />
-      </IconButton>
-    ) : null}
-    {instagram ? (
-      <IconButton href={instagram} target="blank" {...iconButtonProps}>
-        <InstagramIcon sx={(theme) => ({ color: theme.palette.secondary.main })} />
-      </IconButton>
-    ) : null}
-  </Box>
-);
+}) => {
+  const themes = useTheme();
+  const iconStyle = {
+    color: themes.palette.common.white,
+    '&:hover': {
+      color: themes.palette.secondary.main,
+    },
+  };
+  return (
+    <Box {...containerProps}>
+      {linkedIn ? (
+        <IconButton href={linkedIn} target="blank" {...iconButtonProps}>
+          <LinkedInIcon sx={iconStyle} />
+        </IconButton>
+      ) : null}
+      {facebook ? (
+        <IconButton href={facebook} target="blank" {...iconButtonProps}>
+          <FacebookIcon sx={iconStyle} />
+        </IconButton>
+      ) : null}
+      {twitter ? (
+        <IconButton href={twitter} target="blank" {...iconButtonProps}>
+          <TwitterIcon sx={iconStyle} />
+        </IconButton>
+      ) : null}
+      {instagram ? (
+        <IconButton href={instagram} target="blank" {...iconButtonProps}>
+          <InstagramIcon sx={iconStyle} />
+        </IconButton>
+      ) : null}
+    </Box>
+  );
+};
 
 export default SocialLinksContainer;
